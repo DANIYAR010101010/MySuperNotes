24a1ecc1d2868fd89f29adb53dc2bd3daddf4913
DANIYAR010101010 <daniaresenbekov61@gmail.com>
2023-02-28 19:16:12 +0300

    first commit

---
+++ app/src/main/java/com/io/muhsin/mysupernotes/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.io.muhsin.mysupernotes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.io.muhsin.mysupernotes.ui.theme.MySuperNotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MySuperNotesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MySuperNotesTheme {
+        Greeting("Android")
+    }
+}
